d991f8587382d061fb7d00d7ff6ebd92df1d55de
Sairam <31530047+Asairam@users.noreply.github.com>
2020-08-20 18:25:43 +0530

    bydefault open home page,  home page call items api,

--- src/header/header.js
+++ src/header/header.js
@@ -4,8 +4,8 @@ import About from '../about/about';
 import Signup from '../signup/signup';
 import Login from '../login/login';
 import Profile from '../profile/profile';
-import {ToastContainer, ToastStore} from 'react-toasts';
-import { BrowserRouter as Router, Switch, Route, Link,NavLink  } from 'react-router-dom';
+import { ToastContainer, ToastStore } from 'react-toasts';
+import { BrowserRouter as Router, Switch, Route, Link, NavLink } from 'react-router-dom';
 class Header extends Component {
     constructor() {
         super()
@@ -17,65 +17,66 @@ class Header extends Component {
     logout(event) {
         event.preventDefault();
         localStorage.removeItem('user_details');
-        this.setState({localstoragedata:null})
+        localStorage.removeItem('token');
+        this.setState({ localstoragedata: null })
         window.location.href = '/';
         ToastStore.success('Hey, You have logout successfully. !');
     }
-    render(){
-        return(
+    render() {
+        return (
             <div className="Head">
-            <Router>                
-              <div className="clswrapper">
-                    <div id="header" className="clearfix">
-                        <div id="selLogo" className="clsFloatLeft">
-                        <h1><Link to="">Business Co</Link></h1>
-                        </div>
-                        <div className="clsMenu clsFloatRight">
-                        <ul>
-                            { this.state.localstoragedata !== null ?
-                                <span>
+                <Router>
+                    <div className="clswrapper">
+                        <div id="header" className="clearfix">
+                            <div id="selLogo" className="clsFloatLeft">
+                                <h1><Link to="">Business Co</Link></h1>
+                            </div>
+                            <div className="clsMenu clsFloatRight">
+                                <ul>
                                     <li><NavLink to={'/home'} activeClassName="active">Home</NavLink></li>
-                                    <li><NavLink to={'/about'} activeClassName="active">About us</NavLink></li>
-                                    <li><Link to="" >Services</Link></li>
-                                    <li><Link to="">Solutions</Link></li>
-                                    <li className="clsNoBorder"><NavLink to={'/profile'} activeClassName="active">Profile</NavLink></li>
-                                </span>
-                            : null }
-                            <li  className="clsNoBorder">
-                                { !this.state.localstoragedata ? <NavLink to={'/login'} ><button type="button" className="btn  btn-primary login">Login</button></NavLink> : null }
-                                { this.state.localstoragedata !== null ? <button type="button" className="btn  btn-default login" onClick={this.logout} >Logout</button> : null }
-                                &nbsp;&nbsp;
-                              { !this.state.localstoragedata ?  <NavLink to={'/signup'} ><button type="button" className="btn btn-info login">Signup</button></NavLink> : null }
-                                </li>
-                        </ul>
-                       {/*<Switch>
+                                    {this.state.localstoragedata !== null ?
+                                        <span>
+                                            <li><NavLink to={'/about'} activeClassName="active">About us</NavLink></li>
+                                            <li><Link to="" >Services</Link></li>
+                                            <li><Link to="">Solutions</Link></li>
+                                            <li className="clsNoBorder"><NavLink to={'/profile'} activeClassName="active">Profile</NavLink></li>
+                                        </span>
+                                        : null}
+                                    <li className="clsNoBorder">
+                                        {!this.state.localstoragedata ? <NavLink to={'/login'} ><button type="button" className="btn  btn-primary login">Login</button></NavLink> : null}
+                                        {this.state.localstoragedata !== null ? <button type="button" className="btn  btn-default login" onClick={this.logout} >Logout</button> : null}
+                                        &nbsp;&nbsp;
+                              {!this.state.localstoragedata ? <NavLink to={'/signup'} ><button type="button" className="btn btn-info login">Signup</button></NavLink> : null}
+                                    </li>
+                                </ul>
+                                {/*<Switch>
                             <Route  path='/home' component={Home} />
                             <Route  path='/about' component={Home} />
                         </Switch>*/}
+                            </div>
                         </div>
-                    </div>
-                    <div id="banner">
-                        <p className="clsBannerText">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage.</p>
-                        <p className="clsReadmore"><Link to="">Readmore</Link></p>
-                    </div>
-                    { this.state.localstoragedata !== null ?
-                        <span>
+                        <div id="banner">
+                            <p className="clsBannerText">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage.</p>
+                            <p className="clsReadmore"><Link to="">Readmore</Link></p>
+                        </div>
                                 <Route exact path="/" component={Home} />
-                                <Route exact path="/home" component={Home} />
+                                <Route exact path="/home" component={Home} />                                
+                        {this.state.localstoragedata !== null ?
+                            <span>
                                 <Route path="/about" component={About} />
                                 <Route path="/profile" component={Profile} />
-                        </span>
-                        : 
-                        <span>
-                            <Route path="/signup" component={Signup} />
-                            <Route path="/login" component={Login} />
-                            <Route path="*" exact component={Home} />
-                            
-                        </span>
-                         }
-                 </div>
-            </Router>      
-            <ToastContainer store={ToastStore}/>                                 
+                            </span>
+                            :
+                            <span>
+                                <Route path="/signup" component={Signup} />
+                                <Route path="/login" component={Login} />
+                                {/*<Route path="*" exact component={Login} />*/}
+
+                            </span>
+                        }
+                    </div>
+                </Router>
+                <ToastContainer store={ToastStore} />
             </div>
         );
     }
--- src/home/home.js
+++ src/home/home.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
-
+import './home.css';
 class Home extends Component {
-    render(){
+    constructor(props) {
+        super(props);
+        this.state = {
+            itemsArr: []
+        }
+    }
+    componentDidMount() {
+        const apiUrl = 'http://localhost:1234/items/get';
+        fetch(apiUrl, {
+            headers: {
+                'x-access-token': localStorage.getItem('token')
+            }
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                this.setState({itemsArr:data['data']})
+            }
+            );
+    }
+
+    render() {
         return (
-            <div className ="home">
-                hello Home
+            <div className="home">
+                <div>
+                    {this.state.itemsArr.map(name => (
+                        <div className="images" >
+                            <img src={require(`../itemImages/${name.type}.png`)} />
+                            <h5><b>{name.title}</b></h5>
+                        </div>
+                    ))}
+                </div>
             </div>
         );
     }
